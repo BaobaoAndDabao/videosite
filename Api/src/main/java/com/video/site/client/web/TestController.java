01fcf5fc4195102b02edf5e8465959f7d1f8e050
wenzhihaosuot <wenzhihaosuot@163.com>
2018-11-04 12:31:28 +0800

    名称：部署生产 描述：部署生产 提交人： 温志浩 审核人： 范群星
    Signed-off-by: wenzhihaosuot <wenzhihaosuot@163.com>

--- Api/src/main/java/com/video/site/client/web/TestController.java
+++ Api/src/main/java/com/video/site/client/web/TestController.java
@@ -1,19 +1,30 @@
 package com.video.site.client.web;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class TestController {
+public class TestController implements InitializingBean {
 
+    @Autowired
+    private Environment environment;
+//InitializingBean
     @RequestMapping("/hello")
     public String hello(){
 
+        String[] activeProfiles = environment.getActiveProfiles();
 
-        return "Hello Spring Boot";
+        return activeProfiles[0].toString();
     }
 
 
-
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        String[] activeProfiles = environment.getActiveProfiles();
+      //  System.err.println(C);
+    }
 }
